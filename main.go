8ee6409e78be828ed00ce0121c4c9847ba1ac9fb
Tigran <t.mirzakhanian@mail.ru>
2020-10-04 22:26:35 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func Quicksort(ar []int){
+	if len(ar) <= 1 {
+		return
+	}
+	split := devision(ar)
+
+	Quicksort(ar[:split])
+	Quicksort(ar[split:])
+}
+
+func devision(ar []int) int{
+	pivot := ar[len(ar)/2]
+
+	left := 0
+	right := len(ar) - 1
+
+
+	for {
+		for ; ar[left] < pivot; left++ {
+		}
+
+		for ; ar[right] > pivot; right-- {
+		}
+
+		if left >= right {
+			return right
+		}
+
+		swap(ar, left, right)
+	}
+}
+
+func swap(ar []int, i, j int){
+	tmp := ar[i]
+	ar[i] = ar[j]
+	ar[j] = tmp
+}
+
+
+func main() {
+	ar := []int{0,2,17,87,-23,56,134,200,4,5,6,8,10,20,13}
+	Quicksort(ar)
+	fmt.Println(ar)
+}
